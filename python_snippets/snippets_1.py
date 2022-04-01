33fee13387f920cf4fe5232c0b1141dd90d25721
wa-le <adeagboadewale05@gmail.com>
2022-04-01 12:23:49 +0200

    edits1

--- python_snippets/snippets_1.py
+++ python_snippets/snippets_1.py
@@ -15,3 +15,9 @@ print()
 list_of_list = [["a", "b", "c"], [6, 9, 12], ["$", "#", "£"]]
 flattened_list = [j for i in list_of_list for j in i]
 print(flattened_list)
+
+# Reverse the flattened  list
+flattened_list.reverse()
+rev_flattened_list = [i for i in flattened_list]
+print(rev_flattened_list)
+print()
