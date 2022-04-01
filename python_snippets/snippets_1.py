0cf42be6bea0ad85479641890cb761327c921e2d
wa-le <adeagboadewale05@gmail.com>
2022-04-01 12:08:05 +0200

    _

---
+++ python_snippets/snippets_1.py
@@ -0,0 +1,17 @@
+# Print a string n times
+print("This is a separator", "-"*20)
+print("wiggle "*5)
+print()
+
+# split a string
+the_string = "This is not a surprise"
+the_string2 = the_string.split()
+print(the_string2)
+the_string3 = " ".join(the_string2)
+print(the_string3)
+print()
+
+# flatten a list
+list_of_list = [["a", "b", "c"], [6, 9, 12], ["$", "#", "£"]]
+flattened_list = [j for i in list_of_list for j in i]
+print(flattened_list)
